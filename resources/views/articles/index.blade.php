ef04047f650de451d23d830a707fd75da17d2d68
angellee177 <angelriapurnamasari17@gmail.com>
2019-10-18 15:57:32 +0700

    [angel] CRUD done

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,57 @@
+@extends('articles.layout')
+ 
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Blog with Laravel</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('articles.create') }}"> Create New Article</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Created At</th>
+            <th>Updated At</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($articles as $article)
+        <tr>
+            <td>{{ $article->title }}</td>
+            <td>{{ $article->description }}</td>
+            <td>{{ $article->status}}</td>
+            <td>{{ $article->user_id}}</td>
+            <td>{{ $article->created_at}}</td>
+            <td>{{ $article->updated_at}}</td>
+            <td>
+                <form action="{{ route('articles.destroy',$article->id) }}" method="POST">
+   
+                    <a class="btn btn-info" href="{{ route('articles.show',$article->id) }}">Show</a>
+    
+                    <a class="btn btn-primary" href="{{ route('articles.edit',$article->id) }}">Edit</a>
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+    {!! $articles->links() !!}
+      
+@endsection
